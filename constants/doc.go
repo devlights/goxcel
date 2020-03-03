dd8e377971bb0c1b9d9539cf3f98ad7a90c49c8c
devlights <gsf.zero1@devlights.onmicrosoft.com>
2020-03-03 19:06:17 +0900

    Update constants/doc.go

--- constants/doc.go
+++ constants/doc.go
@@ -3,4 +3,5 @@
 // REFERENCES::
 //   - https://excel-ubara.com/EXCEL/EXCEL905.html
 //   - https://docs.microsoft.com/ja-jp/office/vba/language/reference/constants-visual-basic-for-applications
+//   - https://docs.microsoft.com/ja-jp/dotnet/api/microsoft.office.interop.excel?view=excel-pia
 package constants
